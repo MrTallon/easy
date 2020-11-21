2e03ac9f0f46f3be2a7b17842669dd6cd1d77e4d
tallon <352420160@qq.com>
2020-11-21 18:14:38 +0800

    日常开发

--- oauth/oauth-server/src/main/java/com/easy/oauth/configuration/WebSecurityConfiguration.java
+++ oauth/oauth-server/src/main/java/com/easy/oauth/configuration/WebSecurityConfiguration.java
@@ -1,7 +1,14 @@
 package com.easy.oauth.configuration;
 
+import com.easy.oauth.service.impl.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * 认证服务器配置
@@ -12,5 +19,31 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
  */
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public class WebSecurityConfiguration {
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServiceBean());
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        // 忽略的访问路径
+        web.ignoring()
+                .antMatchers("/login/**")
+                .antMatchers("/logout/**");
+    }
+
 }
--- oauth/oauth-server/src/main/java/com/easy/oauth/controller/LoginController.java
+++ oauth/oauth-server/src/main/java/com/easy/oauth/controller/LoginController.java
@@ -1,14 +1,15 @@
 package com.easy.oauth.controller;
 
+import com.easy.oauth.common.Header;
 import com.easy.oauth.param.LoginParam;
 import com.easy.oauth.resonse.ResponseResult;
-import com.easy.oauth.service.LoginService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 登录控制器
@@ -22,16 +23,39 @@ import javax.annotation.Resource;
 public class LoginController {
 
     @Resource
-    private LoginService loginService;
+    private HttpServletRequest request;
 
-    @PostMapping("user")
-    public ResponseResult user(@RequestBody LoginParam param) {
-        return ResponseResult.success(loginService.getToken(param.getUserName(), param.getPassword()));
+    @Resource
+    private ILoginService loginService;
+
+    /**
+     * 管理员登录
+     * @param loginParam {@code JSON} {@link LoginParam}
+     * @return {@link ResponseResult}
+     */
+    @PostMapping("admin")
+    public ResponseResult admin(@RequestBody LoginParam loginParam) {
+        return ResponseResult.success(loginService.getToken(loginParam.getUsername(), loginParam.getPassword()));
     }
 
+    /**
+     * 用户登录，登录只是拿 Token
+     * @param loginParam {@code JSON} {@link LoginParam}
+     * @return {@link ResponseResult}
+     */
+    @PostMapping("user")
+    public ResponseResult users(@RequestBody LoginParam loginParam) {
+        return ResponseResult.success(loginService.getToken(loginParam.getUsername(), loginParam.getPassword()));
+    }
+
+    /**
+     * 刷新令牌
+     * @return {@link ResponseResult}
+     */
     @PostMapping("refresh")
     public ResponseResult refresh() {
-//        String token = Header
-        return ResponseResult.success();
+        String token = Header.getAuthorization(request.getHeader("Authorization"));
+        return ResponseResult.success(loginService.refresh(token));
     }
+
 }
